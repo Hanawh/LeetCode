1226db50f01a75579b480c87695dc626a19be500
Dejavu131 <41535875+Dejavu131@users.noreply.github.com>
2019-07-16 20:02:24 +0800

    Update 买卖股票的最佳时机.cpp

--- 初级算法/数组/买卖股票的最佳时机.cpp
+++ 初级算法/数组/买卖股票的最佳时机.cpp
@@ -1,1 +1,29 @@
-
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty()) return 0;
+        int profit=0,buy=prices[0];
+        for(int i=1; i<prices.size(); ++i)
+        {
+            if(prices[i]<buy) buy=prices[i];
+            else
+            {
+                profit += prices[i]-buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+};
+int main()
+{
+    int nums[]={7, 1, 5, 3, 6, 4};
+    vector<int> vec_nums(nums, nums + sizeof(nums) / sizeof(int));
+    int profit = Solution().maxProfit(vec_nums);
+    cout<<profit;
+    return 0;
+}
