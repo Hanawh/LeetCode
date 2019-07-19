08008a70055efc3aa03566524826c6beb66c03ac
Dejavu131 <41535875+Dejavu131@users.noreply.github.com>
2019-07-19 19:03:02 +0800

    Update 反转字符串.cpp

--- 初级算法/字符串/反转字符串.cpp
+++ 初级算法/字符串/反转字符串.cpp
@@ -1,1 +1,27 @@
-
+/*
+输入：["h","e","l","l","o"]
+输出：["o","l","l","e","h"]
+ */
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution
+{
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        for(int i=0; i<n/2; ++i)
+        {
+            char temp=s[i];
+            s[i]=s[n-1-i];
+            s[n-1-i]=temp;
+        }
+    }
+};
+int main()
+{
+    vector<char> s{'h','e','l','l','o'};
+    Solution().reverseString(s);
+    for(auto i:s) cout<<i<<" ";
+    return 0;
+}
