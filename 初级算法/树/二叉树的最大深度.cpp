e108680f39c86616a86060635edac914a586993b
Dejavu131 <41535875+Dejavu131@users.noreply.github.com>
2019-08-21 18:46:55 +0800

    Update 二叉树的最大深度.cpp

--- 初级算法/树/二叉树的最大深度.cpp
+++ 初级算法/树/二叉树的最大深度.cpp
@@ -30,31 +30,5 @@ public:
             return max(leftdepth, rightdepth) + 1;
         }
     }
-
-    TreeNode* buildTree(vector<string> &tree, size_t size, int index, const string& invalid){
-        TreeNode *root;
-        if(index < size){
-            if(tree[index] == invalid){
-                root = nullptr;
-            }
-            else{
-                root = new TreeNode(stoi(tree[index]));
-                root->left = buildTree(tree, size, ++index, invalid);
-		        root->right = buildTree(tree, size, ++index, invalid);
-            }
-        }
-        return root;
-    }
 };
 
-int main(){
-    vector<string> tree;
-    string s;
-    while (cin >> s && s != "end") //end表示输入完毕 null表示空 eg:3 9 20 null null 15 7 end
-    {
-        tree.push_back(s); //创建树序列
-    }
-    TreeNode *binarytree = Solution().buildTree(tree, tree.size(), 0, "null");
-    cout << Solution().maxDepth(binarytree) << endl;
-    return 0;
-}
