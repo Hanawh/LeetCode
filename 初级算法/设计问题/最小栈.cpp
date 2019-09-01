00103c1381838ae4b5acf43af7f97a1cc208502a
Dejavu131 <41535875+Dejavu131@users.noreply.github.com>
2019-09-01 10:32:13 +0800

    Create 最小栈.cpp

---
+++ 初级算法/设计问题/最小栈.cpp
@@ -0,0 +1,50 @@
+/*
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+*/
+#include<iostream>
+#include<stack> 
+
+using namespace std;
+class MinStack{
+public: 
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+        if (s2.empty() || x <= getMin()) s2.push(x);
+    }
+    
+    void pop() {
+        if (s1.top() == getMin()) s2.pop();
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+private:
+    stack<int> s1;
+    stack<int> s2;
+};
+
+int main(){
+   MinStack* obj = new MinStack();
+   obj->push(-2);
+   obj->push(0);
+   obj->push(-3);
+   cout << obj->getMin() << endl;;   
+}
