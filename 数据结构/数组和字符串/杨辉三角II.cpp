3b92c23899f2f5c736d94b20cf55469f846e3a12
Hana <41535875+Hanawh@users.noreply.github.com>
2019-10-22 22:13:43 +0800

    Update 杨辉三角II.cpp

--- 数据结构/数组和字符串/杨辉三角II.cpp
+++ 数据结构/数组和字符串/杨辉三角II.cpp
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res;
+        if(rowIndex < 0) return res;
+        res.assign(rowIndex + 1, 0);
+        res[0] = 1;
+        for(int i = 1; i <= rowIndex; ++i){//行数索引
+            for(int j = i; j >= 1; --j){//第i行有i+1个值 应该从后向前加
+                res[j] = res[j] + res[j-1];
+            }
+        } 
+        return res;
+    }
+};
