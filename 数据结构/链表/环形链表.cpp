1af13c748fddfdef47da67f9f6ec92398427f7ef
Hana <41535875+Hanawh@users.noreply.github.com>
2019-11-12 20:14:51 +0800

    Update 环形链表.cpp

--- 数据结构/链表/环形链表.cpp
+++ 数据结构/链表/环形链表.cpp
@@ -9,16 +9,16 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-    ListNode* slow = head;
-    ListNode* fast = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-    while (slow && fast && fast->next) {
-        slow = slow->next;          
-        fast = fast->next->next;  
-        if (slow == fast) {         
-            return true;
+        while (slow && fast && fast->next) {
+            slow = slow->next;          
+            fast = fast->next->next;  
+            if (slow == fast) {         
+                return true;
+            }
         }
-    }
     return false;   // change return value to fit specific problem
     }
 };
