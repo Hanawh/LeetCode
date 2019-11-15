fffe320473f0e21635a03068800447847c1b407c
Hana <41535875+Hanawh@users.noreply.github.com>
2019-11-15 22:39:25 +0800

    Create 奇偶链表.cpp

---
+++ 数据结构/链表/奇偶链表.cpp
@@ -0,0 +1,29 @@
+/*
+输入: 1->2->3->4->5->NULL
+输出: 1->3->5->2->4->NULL
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr) return head;
+        ListNode* odd = head;
+        ListNode* even = odd->next;
+        ListNode* temp = even;
+        while(even && even->next){
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = temp;
+        return head;
+    }
+};
