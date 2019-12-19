e7be0f3ddfabb140eb26da31da5fa1148532ddd3
Hana <41535875+Hanawh@users.noreply.github.com>
2019-12-19 22:14:35 +0800

    Update 检测大写字母.cpp

--- 字符串/检测大写字母.cpp
+++ 字符串/检测大写字母.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        if(word.size() <= 1) return true;
-        int flag = 0;
-        int islittle = 0;
-        for (size_t i = 0; i < word.size(); i++)
-		{
-			if(i == 0){
-				if(isupper(word[i])) flag = 1;//首字母是大写
-				continue;
-			}
-			if(i == 1){
-                if(islower(word[i])) islittle = 1;
-                continue;
-            }
-            if((islittle and islower(word[i])) or (islittle == 0 and isupper(word[i])))continue;
-            else return false;
-		}
-        if(flag == 0 and islittle == 0) return false;
+        if(word.empty())return true;
+
+        for(int i = 1; word[i]; i++)
+        {
+            if(i!=1 && isupper(word[i]) != isupper(word[i-1]) )
+                return false;
+        }
+
+        if(!isupper(word[0]) && isupper(word[1]))return false;
         return true;
     }
 };
