887a18b16b9340879876ab0ccaf39332df6c2fa3
Hana <41535875+Hanawh@users.noreply.github.com>
2020-01-14 20:22:18 +0800

    Update 使用最小花费爬楼梯.cpp

--- 动态规划/使用最小花费爬楼梯.cpp
+++ 动态规划/使用最小花费爬楼梯.cpp
@@ -10,4 +10,19 @@ public:
         }
         return min(res[len-1],res[len-2]);
     }
-};
+};//8ms 8.8M
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int len = cost.size();
+        int pre = cost[0];
+        int cur = cost[1];
+        for(int i = 2; i < len; ++i){
+            int temp = min(cur,pre)+cost[i]; 
+            pre = cur;
+            cur = temp;
+        }
+        return min(cur,pre);
+    }
+};//4ms 8.9M
