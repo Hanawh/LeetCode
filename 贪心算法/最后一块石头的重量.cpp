28e14d86ab9cc907bcee8972239b12cab91e98c4
Hana <41535875+Hanawh@users.noreply.github.com>
2020-01-15 19:20:28 +0800

    Update 最后一块石头的重量.cpp

--- 贪心算法/最后一块石头的重量.cpp
+++ 贪心算法/最后一块石头的重量.cpp
@@ -11,3 +11,19 @@ public:
         return stones[0];
     }
 };//8ms 8.7M
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> q; //默认是大顶堆
+        for(int v : stones) q.push(v);
+        while(q.size()>=2){
+            int top1 = q.top();q.pop();
+            int top2 = q.top();q.pop();
+            if(top1-top2) q.push(top1-top2);
+        }
+        return q.size() == 1 ? q.top() : 0;
+    }
+};//4ms 8.7M
+
+
