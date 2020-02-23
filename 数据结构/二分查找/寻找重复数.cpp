00053c3e973a5074ebf2411132e1d4ecbefffd1b
Hana <41535875+Hanawh@users.noreply.github.com>
2020-02-23 22:32:11 +0800

    Update 寻找重复数.cpp

--- 数据结构/二分查找/寻找重复数.cpp
+++ 数据结构/二分查找/寻找重复数.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int i = 1, j = nums.size()-1;
-        while(i<j) //[i,j]
+        int i = 1, j = nums.size();
+        while(i<j) //[i,j) 模版二
         {
             int mid = i+(j-i)/2;
             //小于等于mid的数最多有mid个
@@ -11,16 +11,15 @@ public:
                 if(i<=mid) cnt++;
             }
             
-            if(cnt == mid){ //[mid+1,j]
+            if(cnt == mid){ //[mid+1,j)
                 i = mid + 1;
             }
-            else if(cnt > mid){ //[i,mid]
+            else if(cnt > mid){ //[i,mid)
                 j = mid;
             }
             else if(cnt < mid){
                 i = mid + 1;
             }
-            cout<<i<<" "<<j;
         }
         return i;
     }
