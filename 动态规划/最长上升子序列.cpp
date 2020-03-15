ad2edac0c7b8bd66126605cc0bce7b01665700d7
Hana <41535875+Hanawh@users.noreply.github.com>
2020-03-15 19:51:43 +0800

    Create 最长上升子序列.cpp

---
+++ 动态规划/最长上升子序列.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        
+        vector<int> dp(n);
+        for(int i=0; i<n; ++i){
+            dp[i] = 1;
+            for(int j=0; j<i; ++j){
+                if(nums[i]>nums[j]){
+                    dp[i] = max(dp[j]+1, dp[i]);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
