15b0be63dc81aedb1bc85d99490d1494a95aff86
Hana <41535875+Hanawh@users.noreply.github.com>
2020-04-20 10:32:21 +0800

    Create N叉树的前序遍历.cpp

---
+++ 数据结构/N叉树/N叉树的前序遍历.cpp
@@ -0,0 +1,35 @@
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    vector<int> res;
+    vector<int> preorder(Node* root) {
+        _preorder(root);
+        return res;
+    }
+
+    void _preorder(Node* root){
+        if(root==nullptr) return;
+        res.push_back(root->val);
+        if(root->children.size()==0) return;
+        int len = root->children.size();
+        for(int i=0; i<len; ++i){
+            _preorder(root->children[i]);
+        }
+    }
+};
