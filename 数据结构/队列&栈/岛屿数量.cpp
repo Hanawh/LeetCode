c0efbe1461c07b0a72efeb1cc9500ba0822b6c09
Hana <41535875+Hanawh@users.noreply.github.com>
2020-04-20 10:06:43 +0800

    Update 岛屿数量.cpp

--- 数据结构/队列&栈/岛屿数量.cpp
+++ 数据结构/队列&栈/岛屿数量.cpp
@@ -15,15 +15,12 @@ Input:
 Output: 3
 */
 
-#include<iostream>
-#include<vector>
-#include<queue>
-using namespace std;
 class Solution {
 public:
     int numIslands(vector<vector<char> >& grid) {
-        queue<pair<int, int> > q;
+        stack<pair<int, int> > q;
         if(grid.empty()) return 0;
+        int dir[4][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
         int res=0, row = grid.size(), col = grid[0].size();
         for(int i = 0; i < row; ++i){
             for(int j = 0; j < col; ++j){
@@ -32,23 +29,23 @@ public:
                     res++;
                     //探索所有邻接陆地
                     while(!q.empty()){
-                        pair<int, int> temp = q.front();
+                        auto [x, y] = q.top();
                         q.pop();
-                        int x = temp.first;
-                        int y = temp.second;
                         if(grid[x][y]=='0') continue;
                         //将该陆地的所有邻接陆地入队
                         grid[x][y] = '0'; //已探索
-                        if(isIsland(x-1,y,row,col)) q.push(make_pair(x-1,y));
-                        if(isIsland(x+1,y,row,col)) q.push(make_pair(x+1,y));
-                        if(isIsland(x,y-1,row,col)) q.push(make_pair(x,y-1));
-                        if(isIsland(x,y+1,row,col)) q.push(make_pair(x,y+1));
+                        for(int i=0; i<4; ++i){
+                            int newx = x+dir[i][0];
+                            int newy = y+dir[i][1];
+                            if(isIsland(newx, newy, row, col)) q.push(make_pair(newx, newy));
+                        }
                     }
                 }
             }
         }
         return res;
     }
+
     bool isIsland(int x, int y, int row, int col){
         if(x >= 0 && x < row && y >= 0 && y < col) return true;
         return false;
