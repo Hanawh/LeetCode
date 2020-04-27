f95e5ad81fdb41efe77838c13b434afadacd7c04
Hana <41535875+Hanawh@users.noreply.github.com>
2020-04-27 13:48:58 +0800

    Update 搜索旋转排序数组.cpp

--- 数据结构/二分查找/搜索旋转排序数组.cpp
+++ 数据结构/二分查找/搜索旋转排序数组.cpp
@@ -1,3 +1,4 @@
+//方法一
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -27,3 +28,36 @@ public:
         return -1;
     }
 };//33
+
+//方法二
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int len = nums.size();
+        if(len == 0) return -1;
+        if(len == 1 and nums[0]!=target) return -1;
+        int left = 0, right = len - 1;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[mid] < nums[right]){ //[mid,right]有序-->[mid+1,right]有序
+                if(target >= nums[mid+1] and target <= nums[right]){
+                    left = mid + 1;//[mid+1,right]
+                }
+                else right = mid;//[left,mid]
+            }
+            else if(nums[mid] > nums[right]){//[left,mid]有序
+                if(target >= nums[left] and target <= nums[mid]){ //左半边有序
+                    right = mid;//[left,mid]
+                }
+                else left = mid + 1;//[mid+1,right]
+            }
+            //判断收缩行为：
+            //第一种情况：int mid = (left + right + 1) >>> 1
+            //第二种情况：int mid = (left + right) >>> 1
+            //修改第一种情况 假定[mid+1,right]有序
+        }
+        if(nums[left]==target) return left;
+        return -1;
+    }
+};//100%
