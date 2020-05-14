72c37e84a554a8f15e6248eb56374f2bbec7ef92
Hana <41535875+Hanawh@users.noreply.github.com>
2020-05-14 13:36:59 +0800

    Create 最大正方形.cpp

---
+++ 动态规划/最大正方形.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if(matrix.size() == 0 || matrix[0].size() == 0) return 0;
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> f(m, vector<int>(n));
+        int res = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if(matrix[i][j] == '1'){
+                    if(i == 0 || j == 0) f[i][j] = 1;
+                    else{
+                        f[i][j] = min({f[i][j - 1], f[i - 1][j], f[i - 1][j - 1]}) + 1;
+                    }
+                }
+                res = max(res, f[i][j]);
+            }
+        }
+        return res*res;
+    }
+};
