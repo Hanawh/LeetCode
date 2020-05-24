62dc55455200675c4766acc06d9f62ed7e525136
Hana <41535875+Hanawh@users.noreply.github.com>
2020-05-24 21:51:14 +0800

    Create 实现 Trie (前缀树).cpp

---
+++ 数据结构/前缀树/实现 Trie (前缀树).cpp
@@ -0,0 +1,53 @@
+class Trie {
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        isEnd = false;
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        Trie* node = this;
+        for(auto &c:word){
+            if (node->next[c-'a'] == nullptr) {
+                node->next[c-'a'] = new Trie();
+            }
+            node = node->next[c-'a'];
+        }
+        node->isEnd = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Trie* node = this;
+        for(auto &c:word){
+            if (node->next[c-'a'] == nullptr) {
+                return false;
+            }
+            node = node->next[c-'a'];
+        }
+        return node->isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Trie* node = this;
+        for(auto &c:prefix){
+            if (node->next[c-'a'] == nullptr) {
+                return false;
+            }
+            node = node->next[c-'a'];
+        }
+        return true;
+    }
+private:
+    bool isEnd;
+    Trie* next[26] = {nullptr};
+};
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
