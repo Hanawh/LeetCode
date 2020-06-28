26e70867045d21f4b34074284807885e0ffa4754
Hana <41535875+Hanawh@users.noreply.github.com>
2020-06-28 21:20:56 +0800

    Update 长度最小的子数组.cpp

--- 数据结构/数组和字符串/长度最小的子数组.cpp
+++ 数据结构/数组和字符串/长度最小的子数组.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
+        int len = nums.size();
+        if(len==0) return 0;
+        vector<int> t(len+1);
+        for(int i=1; i <= len; ++i){
+            t[i] = nums[i-1] + t[i-1];
+        }
+
+        int res = INT32_MAX;
+        for(int i=0; i <= len-1; ++i){
+            int left = i+1, right = len;
+            int target = s + t[i];
+            while(left < right){
+                int mid = left + (right - left)/2;
+                if(t[mid] < target) left = mid + 1;
+                else right = mid;
+            }
+            if(t[left] >= target) res = min(res, left-i);
+        }
+        if(res == INT32_MAX) return 0;
+        return res;
+    }
+}; //O(nlogn) 二分法
+
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
         int len=nums.size();
         int r=0, l=0; //r在框外 l在框内
         int sum=0;
@@ -17,4 +43,4 @@ public:
         if(minlen==len+1) return 0;
         return minlen;
     }
-};
+}; //双指针
