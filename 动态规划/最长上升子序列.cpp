9ada367b94a225f3b0a8724f0707faf153c5a350
Hana <41535875+Hanawh@users.noreply.github.com>
2020-07-28 15:19:08 +0800

    Update 最长上升子序列.cpp

--- 动态规划/最长上升子序列.cpp
+++ 动态规划/最长上升子序列.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        if(n==0) return 0;
-        
-        vector<int> dp(n);
-        for(int i=0; i<n; ++i){
-            dp[i] = 1;
+        int len = nums.size();
+        vector<int> dp(len, 1);
+        for(int i=0; i<len; ++i){
             for(int j=0; j<i; ++j){
-                if(nums[i]>nums[j]){
-                    dp[i] = max(dp[j]+1, dp[i]);
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i], dp[j]+1);
                 }
             }
         }
-        return *max_element(dp.begin(), dp.end());
+        //return *max_element(dp.begin(), dp.end());
+        int res = 0;
+        for(int i=0; i<len; ++i){
+            res = max(res, dp[i]);
+        }
+        return res;
     }
-};
+}; // 时间复杂度O(n^2)
